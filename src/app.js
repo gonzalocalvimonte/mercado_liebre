39e84020aebd731da06fbff9e6af088146277f38
Gonzalo Calvimonte <gonzalocalvimonte@gmail.com>
2022-07-18 21:02:18 -0300

    Arreglo de /home

--- src/app.js
+++ src/app.js
@@ -9,7 +9,7 @@ const public = path.join(__dirname, '../public')
 const statics = express.static(public)
 server.use(statics)
 
-server.get('/home', function(req,res){
+server.get('/', function(req,res){
     let file = path.join(__dirname , '/views/home.html')
     res.sendFile(file)
 });
